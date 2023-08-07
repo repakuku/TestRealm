a578c80d8c86589190b9cad2f8f825c398a825d1
Alexey Turulin <repakuku@icloud.com>
2023-08-07 16:45:39 -0400

    created TaskListViewController and TasksViewController

---
+++ TestRealm/ViewControllers/TaskListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TaskListViewController.swift
+//  TestRealm
+//
+//  Created by Alexey Turulin on 8/7/23.
+//
+
+import UIKit
+
+class TaskListViewController: UITableViewController {
+
+}
---
+++ TestRealm/ViewControllers/TasksViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TasksViewController.swift
+//  TestRealm
+//
+//  Created by Alexey Turulin on 8/7/23.
+//
+
+import UIKit
+
+class TasksViewController: UITableViewController {
+
+}
